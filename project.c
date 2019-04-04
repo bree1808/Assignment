4448dd95ae42fd31c426f82cd76fbd572f825aa1
Bree McLeod <c3328310@uon.edu.au>
2019-04-04 05:14:38 +0000

    Added title, description, usage, how it works

--- project.c
+++ project.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
-
-int main() {
-
-  return 0;
-}
+/* 
+    Title:       Rotation Cipher
+    Description: Encrypts text message by substituting each letter in the message with
+                 a letter a fixed number of places away in the alphabet.
+    Usage:       Please enter the plain text to encrypt: help
+                 The Ciphered text is with (key shift = 3): khos
+    How it works: letters: a = 0, b = 1, c = 2, d = 3, e = 4
+                   
+*/
