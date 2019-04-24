61fff35c22965e7863dcb237f592c0556fef83c3
Bree McLeod <c3328310@uon.edu.au>
2019-04-24 08:14:00 +0000

    new

--- project.c
+++ project.c
@@ -10,13 +10,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <string.h>
 
-void encryptionSubstitution();
-void decryptionSubstitution();
-void quit();
+
+char *encryption(char cipher_text[]);
+char *decryption(char cipher_text[]);
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char rndm[26] = {'k', 'u', 'd', 'p', 't', 'e', 'v', 'j', 's', 'b', 'l', 'w', 'a', 'i', 'z', 'g', 'x', 'c', 'm', 'n', 'f', 'y', 'h', 'r', 'o', 'q'};
+
+size_t strlen(const char *str);
+char cipher_text[1000];
+ 
  
 int main () {
+    char message[1000], ch, substitution_key[26];
+    int i, key;
+    int j;
+    char msg[255];
+    char cipher_text[255];
     
     
     int input;
@@ -30,148 +41,128 @@ int main () {
         printf("\nChoose an option and press enter:   ");
         scanf("%d",&input);
      
-     
-     
     
     switch (input) {
-        case 1:
-            	char message[1000], ch;
-            	int i, key;
-            	
-            	printf("Enter a message to encrypt: ");
-                fgets(message, 1000, stdin);
-            	
-            	printf("Enter key: ");
-            	scanf("%d", &key);
-            	
-            	for(i = 0; message[i] != '\0'; ++i){
-            		ch = message[i];
-            		
-            		if(ch >= 'a' && ch <= 'z'){
-            			ch = ch + key;
-            			
-            			if(ch > 'z'){
-            				ch = ch - 'z' + 'a' - 1;
-            			}
-            			
-            			message[i] = ch;
-            		}
-            		else if(ch >= 'A' && ch <= 'Z'){
-            			ch = ch + key;
-            			
-            			if(ch > 'Z'){
-            				ch = ch - 'Z' + 'A' - 1;
-            			}
-            			
-            			message[i] = ch;
-            		}
-            	}
-            	
-            	printf("Encrypted message: %s", message);
-            	
-            }
-
+        case 1: //encryption rotation
+        	printf("Enter a message to encrypt: ");
+        	fgetc(stdin);
+            fgets(message, 1000, stdin);
+        	
+        	printf("Enter key: ");
+        	scanf("%d", &key);
+        	
+        	for(i = 0; message[i] != '\0'; ++i){
+        		ch = message[i];
+        		
+        		if(ch >= 'a' && ch <= 'z'){
+        			ch = ch + key;
+        			
+        			if(ch > 'z'){
+        				ch = ch - 'z' + 'a' - 1;
+        			}
+        			
+        			message[i] = ch;
+        		}
+        		else if(ch >= 'A' && ch <= 'Z'){
+        			ch = ch + key;
+        			
+        			if(ch > 'Z'){
+        				ch = ch - 'Z' + 'A' - 1;
+        			}
+        			
+        			message[i] = ch;
+        		}
+        	}
+        	
+        	printf("Encrypted message: %s", message);
             break;
+            
         case 2:
-            decryptionRotation();
+            printf("Enter a message to decrypt: ");
+            fgetc(stdin);
+            fgets(message, 1000, stdin);
+        	
+        	printf("Enter key: ");
+        	scanf("%d", &key);
+        	
+        	for(i = 0; message[i] != '\0'; ++i){
+        		ch = message[i];
+        		
+        		if(ch >= 'a' && ch <= 'z'){
+        			ch = ch - key;
+        			
+        			if(ch < 'a'){
+        				ch = ch + 'z' - 'a' + 1;
+        			}
+        			
+        			message[i] = ch;
+        		}
+        		else if(ch >= 'A' && ch <= 'Z'){
+        			ch = ch - key;
+        			
+        			if(ch < 'A'){
+        				ch = ch + 'Z' - 'A' + 1;
+        			}
+        			
+        			message[i] = ch;
+        		}
+        	}
+        	
+        	printf("Decrypted message: %s", message);
+            
             break;
+            
         case 3:
-           encryptionSubstitution();
+        printf("Enter plain text: ");
+        fgetc(stdin);
+        fgets(msg, 255, stdin);
+        
+        printf("Enter a unique key of 26 characters for encryption: "); // KUDPTEVJSBLWAIZGXCMNFYHROQ
+        fgets(substitution_key, 27, stdin);
+    
+            for(i = 0; i < strlen(cipher_text); i++)
+            {
+                for(j = 0; j < 26; j++)
+                {
+                    if(alpha[j] == cipher_text[i])
+                    {
+                        cipher_text[i] = substitution_key[j];
+                        
+                break;
+                    }
+                }
+            }
+                printf("\nEncrypted message: %s", cipher_text);
+                return 0;
             break;
         case 4:
-           decryptionSubstitution();
+        printf("Enter plain text: ");
+        fgetc(stdin);
+        fgets(msg, 255, stdin);
+        
+            substitution_key[j] = alpha[j];
+    
+                for(i = 0; i < strlen(cipher_text); i++)
+                {
+                    for(j = 0; j < 26; j++)
+                    {
+                        if(rndm[j] == cipher_text[i])
+                        {
+                            cipher_text[i] = alpha[j];
+                            break;
+                        }
+                    }
+                }
+                printf("\nEncrypted message: %s", cipher_text);
+                return 0;
+                
             break;
         case 5:
-            quit();
+            
             break;
     }
          
 }
-void encryptionRotation();
-	char message[1000], ch;
-	int i, key;
-	
-	printf("Enter a message to encrypt: ");
-    fgets(message, 1000, stdin);
-	
-	printf("Enter key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch + key;
-			
-			if(ch > 'z'){
-				ch = ch - 'z' + 'a' - 1;
-			}
-			
-			message[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch + key;
-			
-			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	printf("Encrypted message: %s", message);
-	
-}
 
-void decryptionRotation(){
-char message[1000], ch;
-	int i, key;
 	
-	printf("Enter a message to decrypt: ");
-    fgets(message, 1000, stdin);
-	
-	printf("Enter key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch - key;
-			
-			if(ch < 'a'){
-				ch = ch + 'z' - 'a' + 1;
-			}
-			
-			message[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch - key;
-			
-			if(ch < 'A'){
-				ch = ch + 'Z' - 'A' + 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	printf("Decrypted message: %s", message);
-}
 
-
-void encryptionSubstitution(){
-printf("\nStarting new game....");
-}
- 
- 
-void decryptionSubstitution(){
-printf("\nChoose from a save file");
-}
-
- 
-void quit(){
-    printf("\nThanks for playing.../n");
-
-}
